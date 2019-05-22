b04ce5cd45190a73b58f0a26500d0cad2f71c182
Shehnazdeep <kaur6200@sheridancollege.ca>
2019-05-22 09:34:45 -0400

    Commit

---
+++ src/studentweek3/Student.java
@@ -0,0 +1,45 @@
+/*
+ * Shehnazdeep Kaur
+ * Student Id- 991539256
+ * PROG 24178-P05
+ */
+package studentweek3;
+
+/**
+ *
+ * @author brata
+ */
+public class Student {
+    
+    private int ID;
+    private String firstName;
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+}
---
+++ src/studentweek3/StudentWeek3.java
@@ -0,0 +1,24 @@
+/*
+ * Shehnazdeep Kaur
+ * Student Id- 991539256
+ * PROG 24178-P05
+ */
+package studentweek3;
+
+/**
+ *
+ * @author brata
+ */
+public class StudentWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Student s1 =new Student();
+        s1.setFirstName("John");
+        s1.setID(4);
+    }
+    
+}
